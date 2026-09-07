f6d074cfe126bc4afd9a09be63210352988a8693
jose antonio <joseantonioalmeida217@gmail.com>
2026-09-06 23:01:19 -0300

    interpolação

---
+++ fundamentos/interpolacao.dart
@@ -0,0 +1,11 @@
+main() {
+  String nome = "João";
+  String status = "Aprovado";
+  double nota = 9.2;
+
+  print(
+    nome + " está " + status + " porque tirou nota " + nota.toString() + "!!",
+  );
+  print("$nome está $status, porque tirou nota $nota !!");
+  print("1 + 1 = ${1 + 1}");
+}
