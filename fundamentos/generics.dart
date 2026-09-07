284cb1ffb547ac7a8c260eff13bfe7d03a5ddda3
jose antonio <joseantonioalmeida217@gmail.com>
2026-09-06 23:13:37 -0300

    generics

---
+++ fundamentos/generics.dart
@@ -0,0 +1,12 @@
+main() {
+  List<String> frutas = ["Maças", 'Banana', "uva"];
+  frutas.add("Goiaba");
+  print(frutas);
+
+  Map<String, int> salarios = {
+    "gerente": 190000,
+    "vendedor": 23444,
+    "estagiario": 600,
+  };
+  print("$salarios");
+}
